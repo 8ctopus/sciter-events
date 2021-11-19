335709c28b260950d5c4434243df31fc5a75ef50
8ctopus <hello@octopuslabs.io>
2021-11-19 09:25:26 +0500

    refactored: project directory architecture updated: sciter sdk to 4.4.8.17bis updated: sciter package manager to 0.2.2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is a [sciter.js](https://sciter.com/) events demo.
 
 - git clone the repository
 - run `install.bat` to download the latest sciter binaries and the sciter package manager
-- install packages `php spm.phar install`
+- install packages `php bin/spm.phar install`
 - run `start.bat`
 - to refresh the app after changes to the html/css click `F5`
 
--- install.bat
+++ install.bat
@@ -1,9 +1,15 @@
-REM sciter 4.4.8.10
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/inspector.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/sciter.dll
+REM sciter 4.4.8.17bis
+SET sciterSDK="be2be52df10ebe501f75901df8ef2467ed710d00"
 
-REM sciter package manager 0.1.6
-curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.6/spm.phar
+mkdir bin\win-x32
 
-pause
+cd bin\win-x32
+
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/inspector.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/sciter.dll
+
+cd ..
+
+REM sciter package manager 0.2.2
+curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.2.2/spm.phar
